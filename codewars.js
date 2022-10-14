243187ff65b679c511f07f0deb635e55b012cb68
Gabriel <96335582+ggavr@users.noreply.github.com>
2022-10-14 15:54:21 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,1 +1,9 @@
+//1
+function findLongestWordLength(str) {
+  let a = str.split(" ");
+  let b = a.map(word => word.length);
+  let c = Math.max(...b)
+  return c;
+}
 
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
