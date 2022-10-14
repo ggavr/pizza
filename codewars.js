fc9d3173d70f23b782a5a766ea36951af4547865
Gabriel <96335582+ggavr@users.noreply.github.com>
2022-10-14 15:59:05 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -7,3 +7,10 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+//2
+function largestOfFour(arr) {
+  let a = arr.map(el => Math.max(...el))
+  return a;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
